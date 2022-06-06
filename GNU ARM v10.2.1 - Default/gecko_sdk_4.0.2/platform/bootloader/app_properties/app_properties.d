51529b10b05ada62f2ff78ee72eafe8de5f5a1fe
NycoElm <nicolas.elmerich@edu.esiee.fr>
2022-06-06 15:25:49 +0200

    Configuration terminée 16/29

---
+++ GNU ARM v10.2.1 - Default/gecko_sdk_4.0.2/platform/bootloader/app_properties/app_properties.d
@@ -0,0 +1,10 @@
+gecko_sdk_4.0.2/platform/bootloader/app_properties/app_properties.o: \
+ C\:/Users/nicol/SimplicityStudio/SDKs/gecko_sdk/platform/bootloader/app_properties/app_properties.c \
+ c\:\users\nicol\simplicitystudio\sdks\gecko_sdk\platform\common\toolchain\inc\sl_gcc_preinclude.h \
+ C\:\Users\nicol\SimplicityStudio\v5_workspace\soc_empty_NE_PC\config/app_properties_config.h \
+ C\:\Users\nicol\SimplicityStudio\v5_workspace\soc_empty_NE_PC\autogen/sl_application_type.h \
+ c\:\users\nicol\simplicitystudio\sdks\gecko_sdk\platform\bootloader\api\application_properties.h
+c\:\users\nicol\simplicitystudio\sdks\gecko_sdk\platform\common\toolchain\inc\sl_gcc_preinclude.h:
+C\:\Users\nicol\SimplicityStudio\v5_workspace\soc_empty_NE_PC\config/app_properties_config.h:
+C\:\Users\nicol\SimplicityStudio\v5_workspace\soc_empty_NE_PC\autogen/sl_application_type.h:
+c\:\users\nicol\simplicitystudio\sdks\gecko_sdk\platform\bootloader\api\application_properties.h:
